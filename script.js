43f4cdb4970ed0f07aa8b69837f675eb2f445794
solicht88 <sakanaasai@gmail.com>
2024-09-26 21:04:19 -0400

    set up media and works, uploaded images

---
+++ script.js
@@ -0,0 +1,29 @@
+/* ignore this--experimental stuff that may be used laters
+
+let photos = ["../images/cool_pink_friend.jpeg", "../images/solicht_icon_high_res.png", "../images/sunset.png",  "../images/under_water.png"];
+
+function displayGallery(name, gallery) {
+    let current = document.getElementsByName(name);
+    for (let i = 0; i < gallery.length; i++) {
+        const imageLink = document.createElement("a");
+        const image = document.createElement("img");
+        imageLink.href = gallery[i];
+        imageLink.appendChild(image)
+        current.appendChild(imageLink);
+    }
+}
+
+html script that doesn't work yet
+<script>
+    let photos = ["../images/cool_pink_friend.jpeg", "../images/solicht_icon_high_res.png", "../images/sunset.png",  "../images/under_water.png"];
+    let current = document.getElementsById(photos);
+    for (let i = 0; i < photos.length; i++) {
+        const imageLink = document.createElement("a");
+        const image = document.createElement("img");
+        imageLink.href = photos[i];
+        image.src = photos[i];
+        imageLink.appendChild(image);
+        current.appendChild(imageLink);
+    }
+</script>
+*/
